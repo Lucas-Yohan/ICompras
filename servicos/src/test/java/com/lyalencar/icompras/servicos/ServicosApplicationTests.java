cd3e98fdb898369c5d8a8d553537fbb67f0f0c3a
Lucas Yohan <lucalen.y@live.com>
2026-09-08 23:10:38 -0300

    feat: initial structure for clients, products and services

---
+++ servicos/src/test/java/com/lyalencar/icompras/servicos/ServicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyalencar.icompras.servicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
